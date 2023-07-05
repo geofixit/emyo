9743eca2e9c3185beceb308731944f7880aae90e
Edmar Tabamo <geofixit316@gmail.com>
2023-07-05 19:10:33 +0300

    Bump version: 0.0.2 → 1.0.0

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/geofixit/emyo',
-    version='0.0.2',
+    version='1.0.0',
     zip_safe=False,
 )
